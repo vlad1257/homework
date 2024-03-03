e29d6b09523715907ae515b702143375212b2b57
Vlad Galai <160611805+vlad1257@users.noreply.github.com>
2024-03-03 14:23:27 +0300

    Add files via upload

---
+++ homework/src/home_work_1/Task13.java
@@ -0,0 +1,11 @@
+package Home_work_1;
+
+public class Task13 {
+    public static void main (String [] args) {
+        double a = 42.5F;
+        int intA = (int) a;
+        int result = intA & 10;
+        System.out.println(result);
+
+    }
+}
---
+++ homework/src/home_work_1/Task6.java
@@ -0,0 +1,30 @@
+package home_work_1;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[]args) {
+        Scanner console = new Scanner(System.in);
+        String b = "Вася";
+        String c = "Анастасия";
+        String d = "Я тебя так долго ждал";
+        System.out.println("Введите ваше имя");
+        String a = console.nextLine();
+        if (a == b ) {
+
+            System.out.println("Привет!");
+            System.out.print("Я тебя так долго ждал");
+
+        } else  {
+            System.out.println("Добрый день,а вы кто?");
+        }
+
+
+
+
+
+    }
+
+
+}
